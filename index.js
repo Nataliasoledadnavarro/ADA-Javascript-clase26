1e32a562770172987913f3404a2e0d55ebccabd9
Natalia Soledad Navarro <nataliasoledadnavarro@gmail.com>
2021-07-01 14:03:16 -0300

    Sumo ejercicio 3

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
-// EJERCICIO 1 MUMUKI
+// EJERCICIO 3 MUMUKI
+
+let nombre = prompt("¿Cuál es tu nombre?");
+
+let apellido = prompt("¿Cuál es tu apellido?");
+
+let edad = prompt("¿Cuál es tu edad?");
+
+let nacionalidad = prompt("¿Cuál es tu nacionalidad?");
+
+let dni = prompt("¿Cuál es tu número de documento?");
+
+alert("El nuevo perfil fue ingresado al sistema: nombre: " + nombre + ", apellido: " + apellido + ", edad: " + edad + ", nacionalidad: " + nacionalidad + ", número de documento: " + dni);
+
+
+/*EJERCICIO 1 MUMUKI
 
 let nombre = prompt("¿Cuál es tu nombre?");
 
 let apellido = prompt("¿Cuál es tu apellido");
 
-alert("Hola " + nombre + " " + apellido + ", bienvenida a Ada");
+alert("Hola " + nombre + " " + apellido + ", bienvenida a Ada");*/
 
-//EJERCICIO 2 MUMUKI
+
+/*EJERCICIO 2 MUMUKI
 
 let primerGusto = prompt("¿Cuál es el primer gusto?");
 
@@ -15,10 +31,4 @@ let segundoGusto = prompt("¿Cuál es el segundo gusto?");
 let tercerGusto = prompt("¿Cuál es el tercer gusto?");
 
 alert(
-  "Aquí tiene su helado de " +
-    primerGusto +
-    ", " +
-    segundoGusto +
-    " y " +
-    tercerGusto
-);
+"Aquí tiene su helado de " + primerGusto + ", " + segundoGusto +" y " +tercerGusto);*/
