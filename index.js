1ffaec28d0f4805a66d795977d607d5fac5cbc78
Natalia Soledad Navarro <nataliasoledadnavarro@gmail.com>
2021-07-01 12:41:37 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+// EJERCICIO 1 MUMUKI
+
+let nombre = prompt("¿Cuál es tu nombre?");
+
+let apellido = prompt("¿Cuál es tu apellido");
+
+alert("Hola " + nombre + " " + apellido + ", bienvenida a Ada");
+
+//EJERCICIO 2 MUMUKI
+
+let primerGusto = prompt("¿Cuál es el primer gusto?");
+
+let segundoGusto = prompt("¿Cuál es el segundo gusto?");
+
+let tercerGusto = prompt("¿Cuál es el tercer gusto?");
+
+alert(
+  "Aquí tiene su helado de " +
+    primerGusto +
+    ", " +
+    segundoGusto +
+    " y " +
+    tercerGusto
+);
