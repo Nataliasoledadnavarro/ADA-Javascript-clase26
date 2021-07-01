0603dbc1b6edf3567db65cb9ae568a8d04a30a3f
Natalia Soledad Navarro <nataliasoledadnavarro@gmail.com>
2021-07-01 14:32:52 -0300

    Ejercicio 4 y 5

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
-// EJERCICIO 3 MUMUKI
 
-let nombre = prompt("¿Cuál es tu nombre?");
+
+
+///////EJERCICIO 1 MUMUKI/////////////////
+
+/*let nombre = prompt("¿Cuál es tu nombre?");
+
+let apellido = prompt("¿Cuál es tu apellido");
+
+alert("Hola " + nombre + " " + apellido + ", bienvenida a Ada");*/
+
+
+////////EJERCICIO 2 MUMUKI//////////////
+
+/*let primerGusto = prompt("¿Cuál es el primer gusto?");
+
+let segundoGusto = prompt("¿Cuál es el segundo gusto?");
+
+let tercerGusto = prompt("¿Cuál es el tercer gusto?");
+
+alert(
+"Aquí tiene su helado de " + primerGusto + ", " + segundoGusto +" y " +tercerGusto);*/
+
+
+/////EJERCICIO 3 MUMUKI///////
+
+/*let nombre = prompt("¿Cuál es tu nombre?");
 
 let apellido = prompt("¿Cuál es tu apellido?");
 
@@ -10,25 +34,39 @@ let nacionalidad = prompt("¿Cuál es tu nacionalidad?");
 
 let dni = prompt("¿Cuál es tu número de documento?");
 
-alert("El nuevo perfil fue ingresado al sistema: nombre: " + nombre + ", apellido: " + apellido + ", edad: " + edad + ", nacionalidad: " + nacionalidad + ", número de documento: " + dni);
+alert("El nuevo perfil fue ingresado al sistema: nombre: " + nombre + ", apellido: " + apellido + ", edad: " + edad + ", nacionalidad: " + nacionalidad + ", número de documento: " + dni);*/
+
+/////EJERCICIO 4 MUMUKI///////
+
+/*let nombrePlaylist = prompt("¿Cuál es el nombre de la playlist?");
+
+let primeraCancion = prompt("¿Cuál es la primera canción?");
+
+let segundaCancion = prompt("¿Cuál es la segunda canción?");
+
+let terceraCancion = prompt("¿Cuál es la tercera canción?");
+
+alert("Se ha creado la playlist " + nombrePlaylist + " con las canciones " + primeraCancion + ", " + segundaCancion + ", " + terceraCancion)*/
+
+/////EJERCICIO 5 MUMUKI///////
+
+/*let calle = prompt("¿Cuál es la calle?");
+
+let numeroDeCalle = prompt("¿Cuál es el número?");
+
+let departamento = prompt("¿Cuál es el departamento?");
+
+let codigoPostal = prompt("¿Cuál es el código postal?");
+
+let ciudad = prompt("¿Cúal es la ciudad?");
+
+let pais = prompt("¿Cúal es el país?");
+
+alert("La dirección que ha ingresado es: " + calle + " " + numeroDeCalle + " " + departamento + ", " + codigoPostal + ", " + ciudad + ", " + pais);*/
 
 
-/*EJERCICIO 1 MUMUKI
-
-let nombre = prompt("¿Cuál es tu nombre?");
-
-let apellido = prompt("¿Cuál es tu apellido");
-
-alert("Hola " + nombre + " " + apellido + ", bienvenida a Ada");*/
 
 
-/*EJERCICIO 2 MUMUKI
 
-let primerGusto = prompt("¿Cuál es el primer gusto?");
 
-let segundoGusto = prompt("¿Cuál es el segundo gusto?");
 
-let tercerGusto = prompt("¿Cuál es el tercer gusto?");
-
-alert(
-"Aquí tiene su helado de " + primerGusto + ", " + segundoGusto +" y " +tercerGusto);*/
